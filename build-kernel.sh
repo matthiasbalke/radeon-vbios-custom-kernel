58db77cbe8f1cee7bb48982559dedb3fa76a1afc
Matthias Balke <matthias.balke@googlemail.com>
2025-03-17 15:34:26 +0100

    use apt-get in favour of apt

--- build-kernel.sh
+++ build-kernel.sh
@@ -17,13 +17,13 @@ uname -a
 sudo sh -c 'echo "deb-src http://de.archive.ubuntu.com/ubuntu noble main restricted universe⁄ndeb-src http://de.archive.ubuntu.com/ubuntu noble-updates main restricted universe" > /etc/apt/sources.list.d/official-source-package-repositories.list'
 
 # refresh repositories
-time sudo apt update
+time sudo apt-get update
 
 # install kernel sources
-time sudo apt build-dep linux linux-image-unsigned-$kernelVersionToBuild
+time sudo apt-get install build-dep linux linux-image-unsigned-$kernelVersionToBuild
 
 # install required packages to build the ubuntu kernel
-time sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
+time sudo apt-get install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
 
 ls -al
 
