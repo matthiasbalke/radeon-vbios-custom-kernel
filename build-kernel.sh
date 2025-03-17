477c8d72652fe696984c06e18df42f39df7c22f5
Matthias Balke <matthias.balke@googlemail.com>
2025-03-17 15:23:01 +0100

    add deb-src sources

--- build-kernel.sh
+++ build-kernel.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bash -x
+
+# exit after first error
+set -e
 
 # docs: https://wiki.ubuntu.com/Kernel/BuildYourOwnKernel
 
@@ -6,14 +9,23 @@
 kernelVersionToBuild=6.8.0-55-generic
 kernelSourceVersion=i$( echo $kernelVersionToBuild | cut -d\- -f 1)
 
+# CI debugging
+uname -r
+uname -a
+
+# add deb-src sources
+echo "deb-src http://ftp.hosteurope.de/mirror/archive.ubuntu.com noble main restricted universe multiverse⁄ndeb-src http://ftp.hosteurope.de/mirror/archive.ubuntu.com noble-updates main restricted universe multiverse" > /etc/apt/sources.list.d/official-source-package-repositories.list
+
+# refresh repositories
+sudo apt update
+
 # install kernel sources
 sudo apt build-dep linux linux-image-unsigned-$kernelVersionToBuild
 
 # install required packages to build the ubuntu kernel
 sudo apt install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf llvm
 
-# install kernel sources
-
+ls -al
 
 cd $kernelSourceVersion
 
